333ac6b364a4b2cc42080ae0ad41d3165ef06187
ruslan <ryslanfree@yandex.ru>
2014-09-08 22:14:52 +0300

    init 1-st commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruslan
+ * Date: 9/8/14
+ * Time: 9:48 PM
+ */
+
+
---
+++ payment.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruslan
+ * Date: 9/8/14
+ * Time: 9:54 PM
+ */
+namespace library;
+use library\PHPExcel;
+
+class Payment extends PHPExcel{
+
+} 
