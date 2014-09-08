ce3753e9781922a8722ea40bb1d827b90fb26aeb
ruslan <ryslanfree@yandex.ru>
2014-09-08 23:34:40 +0300

    #1 add from

--- view/loadfile.php
+++ view/loadfile.php
@@ -34,25 +34,11 @@
     <div class="jumbotron">
         <form class="form-inline" role="form">
             <div class="form-group">
-                <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
-            </div>
-            <div class="form-group">
                 <div class="input-group">
-                    <div class="input-group-addon">@</div>
-                    <input class="form-control" type="email" placeholder="Enter email">
+                    <input class="form-control" type="file" placeholder="Enter email">
                 </div>
             </div>
-            <div class="form-group">
-                <label class="sr-only" for="exampleInputPassword2">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-            </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> Remember me
-                </label>
-            </div>
-            <button type="submit" class="btn btn-default">Sign in</button>
+            <button type="submit" class="btn btn-default">Отправить</button>
         </form>
     </div>
 
